cf0e9d9b83b962378d7e2460f700eb02f3a2b833
jadhavpratik5433 <jadhavpratik5433@gmail.com>
2025-08-12 16:49:37 +0530

    added password key

--- NewProject/Models/Student.cs
+++ NewProject/Models/Student.cs
@@ -13,6 +13,8 @@ namespace NewProject.Models
 
         [Required]
         public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
         public int Age  { get; set; }
 
     }
